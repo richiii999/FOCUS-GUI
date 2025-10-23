7f3783b677743ae9f40545508672a1dc8bd674cc
richiii999 <richiii@umich.edu>
2025-10-23 15:03:26 -0400

    testing grid layout

---
+++ ExGrid.py
@@ -0,0 +1,37 @@
+import tkinter as tk
+from tkinter import ttk
+
+# main window
+root = tk.Tk()
+root.geometry("240x100")
+root.title('Login')
+
+
+# grid 3x2
+root.rowconfigure(0, weight=1)
+root.rowconfigure(1, weight=1)
+# root.rowconfigure(2, weight=1)
+root.columnconfigure(0, weight=1)
+root.columnconfigure(1, weight=3)
+
+
+# username
+username_label = ttk.Label(root, text="Username:")
+username_label.grid(column=0, row=0, sticky=tk.EW, padx=5, pady=5)
+
+username_entry = ttk.Entry(root)
+username_entry.grid(column=1, row=0, sticky=tk.EW, padx=5, pady=5)
+
+# password
+password_label = ttk.Label(root, text="Password:")
+password_label.grid(column=0, row=1, sticky=tk.EW, padx=5, pady=5)
+
+password_entry = ttk.Entry(root,  show="*")
+password_entry.grid(column=1, row=1, sticky=tk.EW, padx=5, pady=5)
+
+# login button
+login_button = ttk.Button(root, text="Login")
+login_button.grid(column=1, row=3, sticky=tk.E, padx=5, pady=5)
+
+
+root.mainloop()
--- gui.py
+++ gui.py
@@ -2,60 +2,62 @@ import tkinter as tk
 from tkinter import ttk
 
 root = tk.Tk()
-root.title("Image Editor")
+root.geometry("1600x900")
+root.title("FOCUS")
 
-image = tk.PhotoImage(file="test.png")
+testImg = tk.PhotoImage(file="test200x200.png")
 
-# Tools frame
-tools_frame = tk.Frame(root, width=200, height=400, bg="skyblue")
-tools_frame.pack(padx=5, pady=5, side=tk.LEFT, fill=tk.Y)
-tk.Label(
-    tools_frame,
-    text="Original Image",
-    bg="skyblue",
-).pack(padx=5, pady=5)
-thumbnail_image = image.subsample(5, 5)
-tk.Label(tools_frame, image=thumbnail_image).pack(padx=5, pady=5)
+# Grid Layout: 2x2
+root.rowconfigure(0, weight=1)
+root.rowconfigure(1, weight=1)
+root.columnconfigure(0, weight=3)
+root.columnconfigure(1, weight=1)
 
-# Tools and Filters tabs
-notebook = ttk.Notebook(tools_frame)
-notebook.pack(expand=True, fill="both")
+# Grid cell test (works)
+TL = ttk.Label(root, image=testImg)
+TR = ttk.Label(root, image=testImg)
+BL = ttk.Label(root, image=testImg)
+BR = ttk.Label(root, image=testImg)
+TL.grid(column=0, row=0)
+TR.grid(column=0, row=1)
+BL.grid(column=1, row=0)
+BR.grid(column=1, row=1)
 
-tools_tab = tk.Frame(notebook, bg="lightblue")
-tools_var = tk.StringVar(value="None")
-for tool in ["Resizing", "Rotating"]:
-    tk.Radiobutton(
-        tools_tab,
-        text=tool,
-        variable=tools_var,
-        value=tool,
-        bg="lightblue",
-    ).pack(anchor="w", padx=20, pady=5)
+# old frames
+# # Orig Image Grid
+# tools_frame = tk.Frame(root, width=200, height=400, bg="skyblue")
+# tools_frame.pack(padx=5, pady=5, side=tk.RIGHT, fill=tk.Y)
+# tk.Label(
+#     tools_frame,
+#     text="Original Image",
+#     bg="skyblue",
+# ).pack(padx=5, pady=5)
+# thumbnail_image = image.subsample(5, 5)
+# tk.Label(tools_frame, image=thumbnail_image).pack(padx=5, pady=5)
 
-filters_tab = tk.Frame(notebook, bg="lightgreen")
-filters_var = tk.StringVar(value="None")
-for filter in ["Blurring", "Sharpening"]:
-    tk.Radiobutton(
-        filters_tab,
-        text=filter,
-        variable=filters_var,
-        value=filter,
-        bg="lightgreen",
-    ).pack(anchor="w", padx=20, pady=5)
+# # Tools frame
+# tools_tab = tk.Frame(root, bg="lightblue")
+# tools_tab.pack(padx=5, pady=5, side=tk.RIGHT, fill=tk.Y)
+# tools_var = tk.StringVar(value="None")
+# for tool in ["Resizing", "Rotating"]:
+#     tk.Radiobutton(
+#         tools_tab,
+#         text=tool,
+#         variable=tools_var,
+#         value=tool,
+#         bg="lightblue",
+#     ).pack(anchor="w", padx=20, pady=5)
 
-notebook.add(tools_tab, text="Tools")
-notebook.add(filters_tab, text="Filters")
-
-# Image frame
-image_frame = tk.Frame(root, width=400, height=400, bg="grey")
-image_frame.pack(padx=5, pady=5, side=tk.RIGHT)
-display_image = image.subsample(2, 2)
-tk.Label(
-    image_frame,
-    text="Edited Image",
-    bg="grey",
-    fg="white",
-).pack(padx=5, pady=5)
-tk.Label(image_frame, image=display_image).pack(padx=5, pady=5)
+# # Image frame
+# image_frame = tk.Frame(root, width=400, height=400, bg="grey")
+# image_frame.pack(padx=5, pady=5, side=tk.LEFT)
+# display_image = image.subsample(2, 2)
+# tk.Label(
+#     image_frame,
+#     text="Edited Image",
+#     bg="grey",
+#     fg="white",
+# ).pack(padx=5, pady=5)
+# tk.Label(image_frame, image=display_image).pack(padx=5, pady=5)
 
 root.mainloop()
