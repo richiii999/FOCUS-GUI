7a00c20cc04d8627002d0fb302b42409584f06a3
richiii999 <richiii@umich.edu>
2025-11-06 11:39:52 -0500

    action buttons added (no effects yet)

---
+++ tests/ActionButtons.py
@@ -0,0 +1,80 @@
+import sys
+sys.path.append(".") # Test scripts use modules too (run from toplevel tho)
+
+import tkinter as tk
+from tkinter import ttk
+
+from modules import PDFViewer
+
+root = tk.Tk()
+root.title("FOCUS")
+root.geometry('800x600')
+
+## Making the frames
+# Side1 Frame (top right)
+Side1 = tk.Frame(root, width=400, height=400, bg="skyblue", relief='ridge', borderwidth=5)
+Side1Label = tk.Label(Side1, text="Side1Label", bg="skyblue")
+
+# Side2 Frame (Bottom right)
+Side2 = tk.Frame(root, width=400, height=400, bg="skyblue", relief='ridge', borderwidth=5)
+Side2Label = tk.Label(Side2, text="Side2Label", bg="skyblue")
+
+# Big Image frame (left)
+BigImg = tk.Frame(root, width=800, height=800, bg="grey", relief='ridge', borderwidth=5)
+BigImgLabel = tk.Label(BigImg, text="BigImgLabel", bg="grey")
+
+# Action Button Frames
+# Action1 (top left)
+A1 = tk.Button(Side1, text="A1", bg="red", relief='ridge', borderwidth=3)
+# A2 (top mid)
+A2 = tk.Button(Side1, text="A2", bg="green", relief='ridge', borderwidth=3)
+# A3 (top right)
+A3 = tk.Button(Side1, text="A3", bg="blue", relief='ridge', borderwidth=3)
+A4 = tk.Button(Side1, text="A4", bg="yellow", relief='ridge', borderwidth=3)
+A5 = tk.Button(Side1, text="A5", bg="purple", relief='ridge', borderwidth=3)
+A6 = tk.Button(Side1, text="A6", bg="cyan", relief='ridge', borderwidth=3)
+
+
+## Grid placement
+# Root Grid
+root.columnconfigure(0, weight=2)
+root.rowconfigure(0, weight=1)
+root.columnconfigure(1, weight=1)
+root.rowconfigure(1, weight=1)
+
+# BigImg
+BigImg.grid(column=0, row=0, columnspan=1, rowspan=2, sticky='news')
+BigImg.columnconfigure(0, weight=2)
+BigImg.rowconfigure(0, weight=2)
+BigImgLabel.grid(column=0, row=0)
+
+pdf_viewer = PDFViewer.PDFViewer(BigImg)
+pdf_viewer.grid(column=0, row=0, sticky='news')
+
+# Side1
+Side1.grid(column=1, row=0, sticky='news')
+Side1.columnconfigure(0, weight=1)
+Side1.columnconfigure(1, weight=1)
+Side1.columnconfigure(2, weight=1)
+Side1.rowconfigure(0, weight=1)
+Side1.rowconfigure(1, weight=1)
+
+# Side2
+Side2.grid(column=1, row=1, sticky='news')
+Side2.columnconfigure(0, weight=1)
+Side2.rowconfigure(0, weight=1)
+
+Side2Label.grid(column=0, row=0, sticky='news')
+
+## Action buttons
+# A1
+A1.grid(column=0, row=0, sticky='news')
+# A1.rowconfigure(0, weight=1)
+# A1.columnconfigure(0, weight=1)
+A2.grid(column=1, row=0, sticky='news')
+A3.grid(column=2, row=0, sticky='news')
+A4.grid(column=0, row=1, sticky='news')
+A5.grid(column=1, row=1, sticky='news')
+A6.grid(column=2, row=1, sticky='news')
+
+root.mainloop()
