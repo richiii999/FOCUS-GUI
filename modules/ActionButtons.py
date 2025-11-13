82b64f4436aa245309a6308f179b4431ccee274e
richiii999 <richiii@umich.edu>
2025-11-13 12:27:29 -0500

    Button default bindings fix

--- modules/ActionButtons.py
+++ modules/ActionButtons.py
@@ -1,17 +1,13 @@
 import tkinter as tk
 
-# TODO fix BUG where default buttons bindings are only on last button, not themselves
-
 def clone(widget, newParent): # Can't reparent: https://stackoverflow.com/questions/46505982/is-there-a-way-to-clone-a-tkinter-widget 
-    cls = widget.__class__
-    clone = cls(newParent)
+    clone = widget.__class__(newParent)
     for key in widget.configure(): clone.configure({key: widget.cget(key)})
-    
     return clone
 
 class ActionButtons(tk.Frame):
     def __init__(self, master, buttonsCount=9, perRow=3):
-        super().__init__(master) # Required idk
+        super().__init__(master)
 
         self.buttons = []
 
@@ -21,30 +17,32 @@ class ActionButtons(tk.Frame):
             print(f" creating button_{i} @ {currRow},{currCol}")
 
             self.buttons.append(tk.Button(self, text=f"Button_{str(i)}", bg="red", relief='ridge', borderwidth=3))
-            self.buttons[i].bind('<ButtonPress-1>', lambda e: self.buttons[i].config(state=tk.DISABLED)) # Action Button default bindings
-            self.buttons[i].grid(column=currCol, row=currRow, sticky='news') # Button Placement in Grid
             
+            self.buttons[i].grid(column=currCol, row=currRow, sticky='news') # Button Placement in Grid
             self.columnconfigure(currCol, weight=1) # Grid resize to fit new button
             self.rowconfigure(currRow, weight=1)
         
-        # Last row of buttons resized to fill empty space ex. [1][2][_][_] -> [ 1 ][ 2 ]
-        numInLastRow = buttonsCount % perRow
-        lastRowPos = int(buttonsCount/perRow)
+        numInLastRow = buttonsCount % perRow  # Last row of buttons resized to fill empty space 
+        lastRowPos = int(buttonsCount/perRow) # ex. [1][2][_][_] -> [ 1 ][ 2 ]
 
-        if (numInLastRow):
-            # print(f"{numInLastRow} in last row (row {lastRowPos}) need resizing")
-
-            lastRow = tk.Frame(self)
+        if (numInLastRow): # Create frame specifically for the last row
+            lastRow = tk.Frame(self) 
             lastRow.grid(column=0, row=lastRowPos, columnspan=perRow, sticky='news')
             lastRow.rowconfigure(0, weight=1)
-            for i in range(numInLastRow): lastRow.columnconfigure(i, weight=1)
 
-            col = 0
             for i in range(buttonsCount - (numInLastRow), buttonsCount): 
                 print(f" resizing button_{i}")
                 self.buttons[i].grid_forget() # unGrid the last row of buttons
                 self.buttons[i] = clone(self.buttons[i], lastRow)  
-                self.buttons[i].grid(row=0, column=col, sticky='news')
-                col += 1
-
-        self.grid(column=0, row=0, sticky='news') # Auto Self-placement
+                
+                lastRow.columnconfigure(i, weight=1) # Grid new buttons
+                self.buttons[i].grid(row=0, column=i, sticky='news')
+            
+        # Default bindings, must be done after replacing the last row. Clone() doesnt preserve bindings I guess
+        for i in range(buttonsCount): self.buttons[i].bind('<ButtonPress-1>', self.defaultAction) 
+        
+        self.grid(column=0, row=0, sticky='news') # Auto Self-placement within parent frame
+    
+    def defaultAction(self, buttonEvent): # Default button action: Disable the button on click
+        print(f"Button pressed: {buttonEvent.widget}")
+        buttonEvent.widget.config(state=tk.DISABLED)
