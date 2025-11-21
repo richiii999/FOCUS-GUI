aaf5b153c6e69a4667c6dc9d1ab7f0f72f388df6
richiii999 <richiii@umich.edu>
2025-11-21 14:34:05 -0500

    starting to put it all together in main

--- main.py
+++ main.py
@@ -5,14 +5,12 @@ import time
 import subprocess # manages subprocess I/O (ollama / webui servers, sensors, and ffmpeg)
 
 import API # Contains API calls to webui
+import Sensors # Controls the face+gaze trackers
 
 def EndStudySession(): # Writes the response to summaryPrompt into the StudyHistory.txt file
     print('\nEnding study session...')
 
-    print('Stopping sensors...')
-    ffmpeg.terminate() # ffmpeg sometimes doesnt terminate, so just spam it 
-    for s in sensors[1:]: s.terminate()
-    for f in procs.values(): f.close() 
+    Sensors.StopSensors()
 
     # with open('./KB/StudyHistory.txt', 'a') as f1, open('./LLM/SummaryPrompt.txt', 'r') as p: 
     #     print('Generating Summary...')
@@ -30,32 +28,20 @@ def EndStudySession(): # Writes the response to summaryPrompt into the StudyHist
     # subprocess.run("rm ./.open-webui/uploads/*", shell=True)
     # subprocess.run("cd ./.open-webui/vector_db && rm -r `ls | grep -v 'chroma.sqlite3'`", shell=True)
 
-    print("\nExiting...\n")
+    print("\nExiting FOCUS...\n")
 
-def Sense() -> str: # Gather output from the sensors
-    sensorData = f"Time = {int(time.time() - startTime)} minutes, Aggregated Sensor data:\n"
-    for f in procs.values(): # Get most recent output per sensor 
-        f.seek(0)
-        sensorData += f.readlines()[-1]
-    return sensorData
 
-startTime, initDelay, iterDelay = time.time(), 3, 5 # Timing delays
 
-print("Starting FFMPEG...") ### Sensors & Subprocesses # Setup virtual cam devices and split original cam input to them
-ffmpeg = subprocess.Popen('ffmpeg  -i /dev/video0 -f v4l2 -vcodec rawvideo -s 640x360 /dev/video8 -f v4l2 -vcodec rawvideo -s 640x360 /dev/video9 -loglevel quiet'.split(), stdin=subprocess.DEVNULL)
-time.sleep(2) # Couple sec buffer for ffmpeg to start 
+# Hmm might need to multithread this or something, 
+# sensors has a inf while loop but so does GUI
+try:
+    Sensors.StartSensors()
+    while True: 
+        print(Sensors.Sense())
+        time.sleep(Sensors.iterDelay)
 
-procs = { # {path : Log file}, sensor output is periodically read from here and given to the AI
-    'python ./Sensors/PythonFaceTracker/main.py'    : open('./SensorLogs/faceTracker.txt', 'r+'),
-    'python ./Sensors/PythonGazeTracker/example.py' : open('./SensorLogs/gazeTracker.txt', 'r+')
-}
+except KeyboardInterrupt: Sensors.StopSensors()
 
-for f in procs.values(): 
-    f.truncate(0) # Empty old logs
-    f.write('The user seems focused\n') # Placeholder first log entry
-
-print("Starting sensors...") # Sensor processes which record data to be passed to the AI
-sensors = [subprocess.Popen(path.split(), stderr=subprocess.DEVNULL, stdout=log, stdin=subprocess.DEVNULL) for path,log in procs.items()]
 
 
 import GUI # Start the GUI
---
+++ modules/Sensors.py
@@ -0,0 +1,38 @@
+# Sensors.py
+# Starts ffmpeg + sensors via subprocess
+
+import time
+import subprocess # manages subprocess I/O (ollama / webui servers, sensors, and ffmpeg)
+
+startTime, initDelay, iterDelay = time.time(), 3, 5 # Timing delays
+
+ffmpeg = None # Set on StartSensors()
+procs = { # {path : Log file}, sensor output is periodically read from here and given to the AI
+    'python ./Sensors/PythonFaceTracker/main.py'    : open('./SensorLogs/faceTracker.txt', 'r+'),
+    'python ./Sensors/PythonGazeTracker/example.py' : open('./SensorLogs/gazeTracker.txt', 'r+')
+}
+
+for f in procs.values(): 
+    f.truncate(0) # Empty old logs
+    f.write('The user seems focused\n') # Placeholder first log entry
+
+def StopSensors():
+    print('Stopping sensors...')
+    ffmpeg.terminate()
+    for s in sensors[1:]: s.terminate()
+    for f in procs.values(): f.close() 
+
+def Sense() -> str: # Gather output from the sensors
+    sensorData = f"Time = {int(time.time() - startTime)} minutes, Aggregated Sensor data:\n"
+    for f in procs.values(): # Get most recent output per sensor 
+        f.seek(0)
+        sensorData += f.readlines()[-1]
+    return sensorData
+
+def StartSensors():
+    print("Starting FFMPEG...") ### Sensors & Subprocesses # Setup virtual cam devices and split original cam input to them
+    ffmpeg = subprocess.Popen('ffmpeg  -i /dev/video0 -f v4l2 -vcodec rawvideo -s 640x360 /dev/video8 -f v4l2 -vcodec rawvideo -s 640x360 /dev/video9 -loglevel quiet'.split(), stdin=subprocess.DEVNULL)
+    time.sleep(2) # Couple sec buffer for ffmpeg to start 
+
+    print("Starting Sensors...") # Sensor processes which record data to be passed to the AI
+    sensors = [subprocess.Popen(path.split(), stderr=subprocess.DEVNULL, stdout=log, stdin=subprocess.DEVNULL) for path,log in procs.items()]
